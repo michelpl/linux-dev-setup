5015dc5e9d81a7151b5c3aaf9868b6e98ef924ff
Michel Lima <michelpl@yahoo.com.br>
2025-06-25 19:11:12 -0300

    Add new aliases for Zsh configuration and GitHub Copilot

--- Ubuntu/configs/aliases.zsh
+++ Ubuntu/configs/aliases.zsh
@@ -1,5 +1,6 @@
 # General
 alias upgrade="sudo apt update -Y && sudo apt upgrade -Y"
 alias refresh-zsh="source ~/.zshrc"
-alias zshrc="nano ~/.zshrc"
+alias aliases="nano ~/.zshrc"
 alias gh-repos="gh repo list"
+alias copilot="gh copilot"
