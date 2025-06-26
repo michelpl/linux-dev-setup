5773a0b62dc8901ac356e5efec8d8fc6d9fcf0a7
Michel Lima <michelpl@yahoo.com.br>
2025-06-26 09:52:24 -0300

    Fix upgrade alias

--- Ubuntu/configs/aliases.zsh
+++ Ubuntu/configs/aliases.zsh
@@ -1,5 +1,5 @@
 # General
-alias upgrade="sudo apt update -Y && sudo apt upgrade -Y"
+alias upgrade="sudo apt update -y && sudo apt upgrade -y"
 alias refresh-zsh="source ~/.zshrc"
 alias aliases="nano ~/.zshrc"
 alias gh-repos="gh repo list"
